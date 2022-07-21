5c22a02858c0be39e76998a3ace7de47cfda864f
Joyce Duarte <joyceduartenascimento@gmail.com>
2022-07-21 11:38:39 -0300

    Procurando ajustar a renderização das imagens dos produtos - retirando um ponto (.) do início do caminho de referência da imagem

--- src/js/database.js
+++ src/js/database.js
@@ -3,7 +3,7 @@
 let data = [
     {
       id: 1,
-      img: "../src/img/products/jaqueta.svg",
+      img: "./src/img/products/jaqueta.svg",
       nameItem: "Jaqueta Inverno",
       description:
         "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
@@ -13,7 +13,7 @@ let data = [
       tags: ["roupas","jaqueta","jaquetas","inverno","frio","preto","camisetas","camiseta","casacos","casaco","manga longa","jacket","jackets","black","Jacket","Jackets"]
     },{
       id: 2,
-      img: "../src/img/products/gorro.svg",
+      img: "./src/img/products/gorro.svg",
       nameItem: "Gorro Next.js",
       description:
         "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
@@ -23,7 +23,7 @@ let data = [
       tags: ["acessórios","acessório","chapéu","chapéus","boné","bonés","preto","gorro","gorros","inverno","frio","next","next.js","touca","toucas","hat","hats","black","Hat","Hats"]
     },{
       id: 3,
-      img: "../src/img/products/mascara.svg",
+      img: "./src/img/products/mascara.svg",
       nameItem: "Máscara de Tecido",
       description:
         "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
@@ -33,7 +33,7 @@ let data = [
       tags: ["máscara","máscaras","facial","preto","acessórios","acessórios","proteção","covid","mask","masks","Mask","Masks","black"]
     },{
       id: 4,
-      img: "../src/img/products/camiseta_preta.svg",
+      img: "./src/img/products/camiseta_preta.svg",
       nameItem: "Camiseta Básica",
       description:
         "Esta camiseta é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
@@ -43,7 +43,7 @@ let data = [
       tags: ["roupas","preto","camisetas","camiseta","básica","manga curta","camisa","camisas","shirt","shirts","t-shirt","t-shirts","black","basic","basics","Shirt","Shirts","T-shirt","T-shirts"]
     },{
       id: 5,
-      img: "../src/img/products/camiseta_branca.svg",
+      img: "./src/img/products/camiseta_branca.svg",
       nameItem: "Camiseta Manga Curta",
       description:
         "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
@@ -53,7 +53,7 @@ let data = [
       tags: ["roupas","branco","camisetas","camiseta","básica","manga curta","camisa","camisas","shirt","shirts","t-shirt","t-shirts","white","basic","basics","Shirt","Shirts","T-shirt","T-shirts"]
     },{
       id: 6,
-      img: "../src/img/products/moletom.svg",
+      img: "./src/img/products/moletom.svg",
       nameItem: "Jaqueta Champion",
       description:
         "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de pol...",
